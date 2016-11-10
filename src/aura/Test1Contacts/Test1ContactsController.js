5f288e05ab7e953c4aac9870042b5d0967aac43e
Viktor S. Yaremenko <vyaremenko.advcomm@gmail.com>
2016-11-10 16:17:52 +0200

    + remove obsolete + add search by name + add show contact list + composition

--- src/aura/Test1Contacts/Test1ContactsController.js
+++ src/aura/Test1Contacts/Test1ContactsController.js
@@ -2,54 +2,26 @@
  * Created by viktor on 07.11.16.
  */
 ({
-     /*showSpinner : function (component, event, helper) {
-         console.log('wait');
-        var spinner = component.find('spinner');
-        var evt = spinner.get("e.toggle");
-        evt.setParams({ isVisible : true });
-        evt.fire();
-    },
-
-    hideSpinner : function (component, event, helper) {
-       console.log('done');
-       var spinner = component.find('spinner');
-       var evt = spinner.get("e.toggle");
-       evt.setParams({ isVisible : false });
-       evt.fire();
-    },*/
-    searchAccount : function (component, event, helper ) {
-        console.log('search account: '+ event.getName()); //
+    searchContacts : function (component, event, helper) {
+        console.log(event);
+        console.log(component.isValid());
+        console.log('search account: '); // + event.getName())
         var sq = component.get('v.searchQuery');
         console.log(sq);
         if ( $A.util.isEmpty(sq) ) {
             return;
         }
-        var spinner = component.find('search-spinner');
-        $A.util.addClass(spinner, 'slds-show');
-        $A.util.removeClass(spinner, 'slds-hide');
-        helper.searchAccounts(component, sq);
+        if ( sq.length < 3 ) { // TODO make cache
+            return;
+        }
+        helper.searchContacts(component, sq, helper);
     },
-    clearSearch : function (component, event, helper ) {
-        console.log('clear account');
-        component.set('v.searchQuery', '');
-        component.set("v.listAccounts", []);
-    },
-    showContacts : function (component, event, helper ) {
-        var accId = event.target ? (event.target.getAttribute('data-account-id') || '') : '';
-        // FIXME check if it real id
-        if ( $A.util.isEmpty(accId) ) return;
-        console.log('Select contacts by account Id: ' + accId);
-        component.set('v.AccountId', accId);
-        component.set('v.searchQuery', event.target.innerText);
-        component.set("v.listAccounts", []);
-    },
-    setPageSize : function (component, event, helper ) {
-        var ipp = event.detail.menuItem.get('v.value');
-        component.set('v.itemsPerPage', ipp);
-        console.log('Set items per page: ' + ipp);
-        console.log(component.get('v.itemsPerPage'));
-        var el = component.find('el_items');
-        el.set('v.visible', false);
-        el.focus();
+    clearSearch : function (component, event, helper) {
+        console.log('clear search');
+        helper.clearData(component);
     }
 })
+
+
+
+
--- src/aura/Test1Contacts/Test1ContactsHelper.js
+++ src/aura/Test1Contacts/Test1ContactsHelper.js
@@ -2,24 +2,40 @@
  * Created by viktor on 08.11.16.
  */
 ({
-      searchAccounts: function(component, searchKey) {
-            if ( $A.util.isEmpty(searchKey) ) return;
-            var action = component.get("c.sFindAccountByName");
-            action.setParams({
-                "searchKey": searchKey
-            });
-            action.setCallback(this, function(response){
-                var state = response.getState();
-                if (component.isValid() && state === "SUCCESS") {
-                    //TODO move to controller
-                    var spinner = component.find('search-spinner');
-                    $A.util.removeClass(spinner, 'slds-show');
-                    $A.util.addClass(spinner, 'slds-hide');
-                    var listAccounts = component.get("v.listAccounts");
-                    listAccounts = response.getReturnValue();
-                    component.set("v.listAccounts", listAccounts);
-                }
-            });
-        $A.enqueueAction(action);
+    showLoader: function(component) {
+        var spinner = component.find('search-spinner');
+        $A.util.addClass(spinner, 'slds-show');
+        $A.util.removeClass(spinner, 'slds-hide');
+    },
+    hideLoader: function(component) {
+        var spinner = component.find('search-spinner');
+        $A.util.removeClass(spinner, 'slds-show');
+        $A.util.addClass(spinner, 'slds-hide');
+    },
+    clearData: function(component) {
+        component.set('v.searchQuery', '');
+        component.set("v.listContacts", []);
+    },
+    searchContacts: function(component, searchKey, helper) {
+        helper.clearData(component);
+        helper.showLoader(component);
+        var action = component.get("c.findContactByName");
+        action.setParams({
+            "searchKey": searchKey,
+            "itemsPerPage": component.get('v.itemsPerPage')
+        });
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            helper.hideLoader(component);
+            if (component.isValid() && state === "SUCCESS") {
+                //TODO move to controller
+                var listContacts = component.get("v.listContacts");
+                listContacts = response.getReturnValue();
+                component.set("v.listContacts", listContacts);
+            } else {
+                console.log(response);
+            }
+        });
+    $A.enqueueAction(action);
       }
 })
