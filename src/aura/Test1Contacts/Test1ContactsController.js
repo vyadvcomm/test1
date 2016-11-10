91d2f85fa43313724358f41e2007b8e2aafdefea
Viktor S. Yaremenko <vyaremenko.advcomm@gmail.com>
2016-11-10 18:08:47 +0200

    + add show contact details (draft) + add navigate to Contact view (draft)

--- src/aura/Test1Contacts/Test1ContactsController.js
+++ src/aura/Test1Contacts/Test1ContactsController.js
@@ -8,6 +8,7 @@
         console.log('search account: '); // + event.getName())
         var sq = component.get('v.searchQuery');
         console.log(sq);
+        helper.searchContacts(component, sq, helper);
         if ( $A.util.isEmpty(sq) ) {
             return;
         }
@@ -17,8 +18,38 @@
         helper.searchContacts(component, sq, helper);
     },
     clearSearch : function (component, event, helper) {
-        console.log('clear search');
+        console.log('clearSearch');
         helper.clearData(component);
+    },
+    navToObject: function (component, event, helper) {
+        console.log('navToObject');
+        console.log(event);
+        var objectId = '0030Y000002w9zdQAA';
+        if ( $A.util.isEmpty(objectId) ) {
+            return;
+        }
+        var urlEvent = $A.get("e.force:navigateToURL");
+        urlEvent.setParams({
+          "url": "/" + objectId
+        });
+        urlEvent.fire();
+    },
+    showDetails: function (component, event, helper) {
+        console.log('showDetails');
+        console.log(event);
+
+        //var objectId = '0030Y000002w9zdQAA';
+        var ct = component.get('v.listContacts')[0];
+        component.set('v.curContact',ct);
+/*
+        if ( $A.util.isEmpty(objectId) ) {
+            return;
+        }
+*/
+    },
+    closeObject: function (component, event, helper) {
+        //component.set('v.contactId','');
+        component.set('v.curContact',[]);
     }
 })
 
